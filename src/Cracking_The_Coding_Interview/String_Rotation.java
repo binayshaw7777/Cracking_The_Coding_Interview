c552686e2123d98dcc60775553765f89e2fc0c16
Binay Shaw <binayshaw7777@gmail.com>
2022-02-05 21:05:55 +0530

    String Rotation Solved

---
+++ src/Cracking_The_Coding_Interview/String_Rotation.java
@@ -0,0 +1,26 @@
+package Cracking_The_Coding_Interview;
+import java.util.Scanner;
+public class String_Rotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two String to check is they're Rotated String or not.");
+        System.out.println("First String: ");
+        String s1 = sc.nextLine();
+        System.out.println("Second String: ");
+        String s2 = sc.nextLine();
+        // s1 = xy = waterbottle && // s2 = yx = erbottlewat
+        //x = wat, y = erbottle
+        System.out.println(isRotation(s1, s2));
+    }
+
+    public static boolean isRotation(String s1, String s2) { //checks if rotated or not
+        int length = s1.length();
+        if(length!=s2.length()) return false;
+        String concat = s1+s1; //concat = wat.erbottlewat.erbottle, s2 = .erbottlewat.  I used . just to show the subtring
+        return isSubstring(concat, s2);
+    }
+
+    public static boolean isSubstring(String concat, String s2) { //checks if s2 is a part of concat string
+        return concat.contains(s2); //String method to check if concat contains s2 in it.
+    }
+}
